8dac3ffed7de81402dcd557d03526c0acc7b0fa4
jelu <jenaehe@yahoo.com>
2023-08-12 21:40:43 -0600

    fixed the flex

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var APIKey = "f7169177f894f761fb58e3a8508669f4";
-var city;
+// var city;
+var fetchButton = document.getElementById("sbutton");
+var cityList = document.getElementById("input");
 
 api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
+
+localStorage.setItem(cityList, "cityName")
+
